c0205d545ce11e3e7f962847cfb0450f038ff3d6
student <student@ip-172-31-60-159.us-west-2.compute.internal>
2021-11-16 18:24:43 -0800

    working on jeopardy

--- src/_03_jeopardy/Jeopardy.java
+++ src/_03_jeopardy/Jeopardy.java
@@ -51,40 +51,48 @@ public class Jeopardy implements ActionListener {
 		frame.setLayout(new BorderLayout());
 
 		// 1. Make the frame show up
-
+		frame.setVisible(true);
 		// 2. Give your frame a title
-
+		frame.setTitle("Jeopardy");
 		// 3. Create a JPanel variable to hold the header using the createHeader method
-
+		JPanel panel=createHeader("Disney Princess");
+		quizPanel.add(panel);
 		// 4. Add the header component to the quizPanel
 
 		// 5. Add the quizPanel to the frame
-
+		frame.add(quizPanel);
 		// 6. Use the createButton method to set the value of firstButton
-
+		firstButton=createButton("200");
 		// 7. Add the firstButton to the quizPanel
-
+		quizPanel.add(firstButton);
 		// 8. Write the code to complete the createButton() method below. Check that your
 		// game looks like Figure 1 in the Jeopardy Handout - http://bit.ly/1bvnvd4.
 
 		// 9. Use the secondButton variable to hold a button using the createButton
 		// method
-
+		secondButton=createButton("400");
 		// 10. Add the secondButton to the quizPanel
-
+		quizPanel.add(secondButton);
 		// 11. Add action listeners to the buttons (2 lines of code)
-
+		secondButton.addActionListener(this);
+		firstButton.addActionListener(this);
+	
 		// 12. Write the code to complete the actionPerformed() method below
 
 		// 13. Add buttons so that you have $200, $400, $600, $800 and $1000 questions
-		
+		thirdButton=createButton("600");
+		quizPanel.add(thirdButton);
+		fourthButton=createButton("800");
+		quizPanel.add(fourthButton);
+		thirdButton.addActionListener(this);
+		fourthButton.addActionListener(this);
 		 /*
 		 * [optional] Use the showImage or playSound methods when the user answers a
 		 * question
 		 */		
 		
 		frame.pack();
-		quizPanel.setLayout(new GridLayout(buttonCount + 1, 3));
+		quizPanel.setLayout(new GridLayout(0,1));
 		frame.add(makeScorePanel(), BorderLayout.NORTH);
 		frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().height,
 				Toolkit.getDefaultToolkit().getScreenSize().width);
@@ -94,14 +102,14 @@ public class Jeopardy implements ActionListener {
 	private JButton createButton(String dollarAmount) {
 		
 		// Create a new JButton
-
+		JButton button=new JButton(dollarAmount);
 		// Set the text of the button to the dollarAmount
-
+		
 		// Increment the buttonCount (this should make the layout vertical)
-
+		dollarAmount+=100;
 		// Return your new button instead of the temporary button
 
-		return new JButton("temporary button");
+		return button;
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -111,7 +119,9 @@ public class Jeopardy implements ActionListener {
 
 		JButton buttonPressed = (JButton) e.getSource();
 		// If the buttonPressed was the firstButton
-
+		if(buttonPressed==firstButton) {
+			askQuestion("what was the name of the princess that had very long hair?", "Rapunzel", 100);
+		}
 			// Call the askQuestion() method
  
 		// Complete the code in the askQuestion() method. When you play the game, the score should change.
